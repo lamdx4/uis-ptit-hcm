eb363fec9737ec9ecf38d1904591342643472f30
TanKhoa1709 <nguyentankhoa170924@gmail.com>
2025-08-08 17:26:47 +0700

    feat(invoices): add viewModel
    add InvoicesViewModel.kt

---
+++ app/src/main/java/lamdx4/uis/ptithcm/ui/more/invoices/InvoicesViewModel.kt
@@ -0,0 +1,40 @@
+package lamdx4.uis.ptithcm.ui.more.invoices
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import lamdx4.uis.ptithcm.data.model.InvoiceResponse
+import lamdx4.uis.ptithcm.data.repository.InvoicesRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class InvoicesViewModel @Inject constructor(
+    private val invoicesRepository: InvoicesRepository
+) : ViewModel() {
+    private val _invoicesState = MutableStateFlow<InvoiceResponse?>(null)
+    val invoicesState = _invoicesState
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+
+    private val _isLoading = MutableStateFlow(false)
+
+    fun loadInvoices() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val result = invoicesRepository.getInvoices()
+            result.onSuccess { invoices ->
+                _invoicesState.value = invoices
+                _errorMessage.value = null
+            }.onFailure { e ->
+                _errorMessage.value = e.message ?: "Không thể tải danh sách hóa đơn"
+            }
+            _isLoading.value = false
+        }
+    }
+
+    fun refreshInvoices() {
+        loadInvoices()
+    }
+}
